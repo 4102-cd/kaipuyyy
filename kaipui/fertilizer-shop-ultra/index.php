36555911a77c6be279f685256f2297e6f7cb66ac
4102-cd <66010914102@msu.ac.th>
2026-02-03 21:51:16 +0700

    Update index.php

--- kaipui/fertilizer-shop-ultra/index.php
+++ kaipui/fertilizer-shop-ultra/index.php
@@ -1,4 +1,9 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/includes/functions.php';
 $pdo = db();
 $cats = $pdo->query("SELECT * FROM categories ORDER BY name")->fetchAll();
